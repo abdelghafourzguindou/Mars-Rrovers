db1993b828e48032960c0c8bc4862c7828924355
azguindou <abdelghafour_zguindou@intuit.com>
2023-02-16 21:59:56 +0100

    delete unused import and dep

--- src/main/java/org/nasa/mars/rovers/RoversApplication.java
+++ src/main/java/org/nasa/mars/rovers/RoversApplication.java
@@ -1,20 +1,8 @@
 package org.nasa.mars.rovers;
 
-import lombok.RequiredArgsConstructor;
-import org.nasa.mars.rovers.model.Heading;
-import org.nasa.mars.rovers.model.Instruction;
-import org.nasa.mars.rovers.model.Plateau;
-import org.nasa.mars.rovers.model.Position;
-import org.nasa.mars.rovers.model.Rover;
-import org.nasa.mars.rovers.service.PlateauService;
-import org.nasa.mars.rovers.service.RoverService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class RoversApplication {
